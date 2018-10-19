710cfac33c59fafadcd5a6339f20b9dba7892009
Felipe Osório <felipe.osorio72@outlook.com.br>
2018-10-19 16:30:44 -0300

    Transform signal to int and sent to c executable

--- translator.py
+++ translator.py
@@ -1,11 +1,18 @@
 import pybrl as brl
 import sys
+import os
 
 with open(sys.argv[1], 'r') as f:
     lines = f.readlines()
 
 result = []
 content = [x.strip() for x in lines]
+for i, line in enumerate(content):
+    if len(line) > 27:
+        content.insert(i+1, line[27:])
+        content[i] = line[:27]
+
+content = [x.ljust(27) for x in content]
 for line in content:
     bra = brl.matrix(brl.braille(line))
     char_lines = []
@@ -17,6 +24,6 @@ for line in content:
     for char_line in char_lines:
         result.append(''.join([str(e) for e in char_line]))
 
-with open(sys.argv[2], 'wb') as f:
-    for line in result:
-        f.write(line + '\n')
+for line in result:
+    for num in (lambda s: map(lambda x: int(x, 2), (lambda ss: [ss[x:x+8] for x in range(0, len(ss)//8 + (len(ss) - len(ss)//8), 8)])(s)))(line):
+        os.system("./{0} {1}".format(sys.argv[2], num))
